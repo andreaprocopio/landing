6d9e33039437f465b7d97e4577f8314c140ed1e0
Andrea Procopio <andrea.procopio@reddoak.com>
2024-03-08 14:33:10 +0100

    init refactor of flowbite components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ProjectsGrid from './components/ProjectsGrid'
+import './App.css';
 
 const App = () => {
   return (
